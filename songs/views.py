cdae7fc5539ff8fbd3a04fa93a3211cf2780ded9
Colleen Stewart <collstewart30@gmail.com>
2022-10-13 14:36:20 -0400

    Registered songs app with admin site

--- songs/views.py
+++ songs/views.py
@@ -1,8 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Song
+from .serializers import SongSerializer
 
 
 @api_view(['GET'])
 def song_list(request):
 
-    return Response('ok')
+    songs = Song.objects.all()
+    serializer = SongSerializer(songs, many=True)
+
+    return Response(serializer.data)
