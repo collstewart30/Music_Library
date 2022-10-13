18be0a3705791e5ac1e4d64aacdc1a158655ff2c
Colleen Stewart <collstewart30@gmail.com>
2022-10-13 13:11:51 -0400

    Created songs serializer

---
+++ songs/serializers.py
@@ -0,0 +1,8 @@
+from pyexpat import model
+from rest_framework import serializers
+from .models import Song
+
+class SongSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Song
+        fields = ['title','artist','album','release_date','genre']
