f87e512e436758e596885fd99118c2f4d981f5eb
Colleen Stewart <collstewart30@gmail.com>
2022-10-13 15:08:17 -0400

    Created view function for get song by id. Added new urls path for song detail view function

--- songs/urls.py
+++ songs/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.song_list)
+    path('',views.song_list),
+    path('<int:pk>/',views.song_detail)
 ]
--- songs/views.py
+++ songs/views.py
@@ -2,12 +2,30 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Song
 from .serializers import SongSerializer
+from rest_framework import status
+
+from songs import serializers
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def song_list(request):
 
-    songs = Song.objects.all()
-    serializer = SongSerializer(songs, many=True)
+    if request.method == 'GET':
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data)
 
-    return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+def song_detail(request, pk):
+    try:
+        song = Song.objects.get(pk=pk)  #look for the pk of this car table and give us one that's equal to that
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
+    except Song.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
